b74b5ce80958009b831dc82beda0aa99789d866a
Arwin-Fabro <flarwingo@gmail.com>
2024-02-05 11:57:29 +0800

    Added Character Script
    Movement (Left and Right)
    Spacebar (Jump)
    Left click to Attack (Have not implemented bullet script)
    Added turret to shoot player when close by

---
+++ Assets/Script/RotateMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMap : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            transform.Rotate(new Vector3(0, 0, -90f));
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            transform.Rotate(new Vector3(0, 0, 90f));
+        }
+
+    }
+}
